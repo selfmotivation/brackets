2275024f2ccdedc6532c31703a99de68568361d4
Selfmotivation <mira55@yandex.ru>
2020-10-19 02:13:36 +0400

    add: the task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = str.split('');
+  let stack = [];
+  let bracketsConf = {};
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    bracketsConf[bracketsConfig[i][0]] = bracketsConfig[i][1];
+  }
+
+  for (let i = 0; i < brackets.length; i++) {
+    if (stack[0] == brackets[i]) {
+      stack.shift();
+    } else if (bracketsConf[brackets[i]]) {
+      stack.unshift(bracketsConf[brackets[i]]);
+    } else {
+      return false;
+    }
+  }
+
+  if (stack.length !== 0) {
+    return false;
+  }
+  return true;
 }
